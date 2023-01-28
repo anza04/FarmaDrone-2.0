d4f97d6a40944453cf89c026bdbe1046ecaae109
anza04 <64538739+anza04@users.noreply.github.com>
2023-01-28 09:49:49 +0100

    2.3

--- src/routes/assistenza.jsx
+++ src/routes/assistenza.jsx
@@ -3,8 +3,15 @@ import '../css/assistenza.css'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import { Button } from '@mui/material';
 
-
 function Assistenza() {
+
+  
+  const Card = (props)=>{
+    return <div className="cardAssistenza">
+      <img src={props.src} alt="" className="imgCard" />
+      <div className="titoloCard">{props.titolo}</div>
+    </div>
+  }
   
   return (
     <div className="Account">
@@ -18,12 +25,15 @@ function Assistenza() {
 
         <div className="contentAssistenza">
           <div className="titoloAssistenza">Per ogni esigenza</div>
-          <Button variant="contained" sx={{width:'60%',height:"5vh",backgroundColor:"grey"}}>CHIAMACI</Button>
-          <div className="cards">
-            <div className="cardAssistenza"></div>
-            <div className="cardAssistenza"></div>
-            <div className="cardAssistenza"></div>
-            <div className="cardAssistenza"></div>
+          <Button variant="contained" sx={{marginTop:"5px",fontWeight:"bold",width:'60%',height:"5vh",backgroundColor:"#D6D6D6", color:"black"}}>
+            <img className='chiamaci' src="/telefono.png" alt=""/>
+            CHIAMACI
+            </Button>
+          <div className="cardsAssistenza">
+            <Card titolo="Spedizione o ordine" src="/spedizione.png"></Card>
+            <Card titolo="Pagamenti e fatture" src="/pagamenti.png"></Card>
+            <Card titolo="Impostazioni account e privacy" src="/privacy.png"></Card>
+            <Card titolo="FarmaDrone Prime" src="/prime.png"></Card>
           </div>
         </div>
 
