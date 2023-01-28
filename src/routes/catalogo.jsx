7915155862e2d84cd0f5112a0cc211c2ec9ed904
anza04 <64538739+anza04@users.noreply.github.com>
2023-01-28 10:33:22 +0100

    2.3.2

--- src/routes/catalogo.jsx
+++ src/routes/catalogo.jsx
@@ -1,8 +1,16 @@
 import { useState } from 'react'
 import '../css/catalogo.css'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { ShowChart } from '@mui/icons-material';
+
+const urlTachipirina = "https://meafarma.it/image/cache/data/pharmadb/012745117-900x900.jpg";
+const urlOki = "https://th.bing.com/th/id/OIP.QkNyhcZ7lYnBjqZsEWKvmwHaHa?w=184&h=184&c=7&r=0&o=5&dpr=1.3&pid=1.7";
+const urlNurofen = "https://th.bing.com/th/id/OIP.pviqZ_9W8FZABRY1iTYhtgHaEc?w=293&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7";
+const urlMomentact = "https://www.bing.com/th?id=OPE.BB%2batqsRvSq7tQ300C300&pid=21.1&w=140&h=140&qlt=100&dpr=1,3"
 
 function Catalogo() {
+
+  const [ShowDetails, SetShowDetails] = useState(0);
 
   const Card = (props)=>
   (
@@ -37,15 +45,14 @@ function Catalogo() {
             </div>
 
             <div className="prodotti">
-              <Link to={"tachipirina"}><Card nome="tachipirina" prezzo="6€" src="public/granulato.png"></Card></Link>
-              <Link to={"okitask"}><Card nome="oki task" prezzo="5€" src="public/okitask.png"></Card></Link>
-              <Link to={"nurofen"}><Card nome="nurofen" prezzo="8€" src="public/nurofen.png"></Card></Link>
-              <Link to={"moment"}><Card nome="moment act" prezzo="6€" src="public/momentact.png"></Card></Link>
-              
-              <Link to={"tachipirina"}><Card nome="tachipirina" prezzo="6€" src="public/granulato.png"></Card></Link>
-              <Link to={"okitask"}><Card nome="oki task" prezzo="5€" src="public/okitask.png"></Card></Link>
-              <Link to={"nurofen"}><Card nome="nurofen" prezzo="8€" src="public/nurofen.png"></Card></Link>
-              <Link to={"moment"}><Card nome="moment act" prezzo="6€" src="public/momentact.png"></Card></Link>
+              <Card nome="tachipirina" prezzo="6€" src={urlTachipirina}></Card>
+              <Link to={"okitask"}><Card nome="oki task" prezzo="5€" src={urlOki}></Card></Link>
+              <Link to={"nurofen"}><Card nome="nurofen" prezzo="8€" src={urlNurofen}></Card></Link>
+              <Link to={"moment"}><Card nome="moment act" prezzo="6€" src={urlMomentact}></Card></Link>
+              <Link to={"tachipirina"}><Card nome="tachipirina" prezzo="6€" src={urlTachipirina}></Card></Link>
+              <Link to={"okitask"}><Card nome="oki task" prezzo="5€" src={urlOki}></Card></Link>
+              <Link to={"nurofen"}><Card nome="nurofen" prezzo="8€" src={urlNurofen}></Card></Link>
+              <Link to={"moment"}><Card nome="moment act" prezzo="6€" src={urlMomentact}></Card></Link>
               
             </div>            
 
