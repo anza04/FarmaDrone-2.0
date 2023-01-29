d0dd5261dcf6fb76dea92cf74406dec8c21f591a
anza04 <64538739+anza04@users.noreply.github.com>
2023-01-29 12:27:12 +0100

    2.3.5 (aggiunta templates)

---
+++ src/routes/template.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import '../css/nomeCosaFate.css'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+function NomeCosaFate() {
+  
+  return (
+    <div className="NomeCosaFate">
+
+        <div className="nav-bar">
+            <div className="bar">
+            <Link to={'/home'} className="home">home</Link>
+            </div>
+            <Link to={'/Account'} className="circleAccount"><div className="account"></div></Link>
+        </div>
+
+
+
+    </div>
+  )
+}
+
+export default NomeCosaFate
+
