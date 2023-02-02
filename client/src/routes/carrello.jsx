26ddbbd95479e36ade821dfd444aa823e01d8213
anza04 <64538739+anza04@users.noreply.github.com>
2023-02-02 15:24:00 +0100

    2.9.2

--- client/src/routes/carrello.jsx
+++ client/src/routes/carrello.jsx
@@ -6,37 +6,15 @@ import { Button } from '@mui/material';
 const urlTachipirina = "https://meafarma.it/image/cache/data/pharmadb/012745117-900x900.jpg";
 const urlInsulina = "https://www.mcguffmedical.com/content/images/thumbs/0010734_insulin-lantus-insulin-glargine-injection-100uml-mdv-10ml-vial.jpeg";
 
+const PrezzoTachi=6;
+const PrezzoInsu=10;
 
 function Carrello() {
 
-  const SchedaCarrello = (props)=>{
-    return  <div className="schedaCarrello">
-
-        <img src={props.src} className="fotoCarrello"/>
-
-        <div className="destraCarrello">
-            <div className="testoCarrello"> {props.nome} <br></br> tot: </div>
-
-            <div className="azioniProdottoCarrello">
-                <Button onClick={()=>document.getElementById("counter").innerText=document.getElementById("counter").innerText-1} variant="contained" sx={{background:"white", color:"black", border:"solid black 1px",width:"30px", height:"30px"    }}>-</Button>
-                <Button id='counter' variant="contained" sx={{background:"#D9D9D9", color:"black", border:"solid black 1px",width:"40px", height:"30px"}}>{props.counter}</Button>
-                <Button onClick={()=>document.getElementById("counter").innerText=document.getElementById("counter").innerText+1} variant="contained" sx={{background:"white", color:"black", border:"solid black 1px",width:"30px", height:"30px"}}>+</Button>
-            </div>
-        </div>
-    </div>    
-  }
-
   const [nTachipirina, setTachi] = useState(1);
   const [nInsu, setInsu] = useState(1);
-
-  const Card = (props)=>
-  (
-    <div className="card">
-      <div className="imgContainerCatalogo"><img className='imgCatalogo' src={props.src}/></div>
-      <div className="nomeProdotto" fontSize="15px">{props.nome}</div>
-      <div className="prezzo">{props.prezzo}</div>
-    </div>
-  );
+  const [showTachi, setShowTachi] = useState(true);
+  const [showInsu, setShowInsu] = useState(true);
   
   return (
     <div className="Carrello">
@@ -53,6 +31,7 @@ function Carrello() {
         <div className="contentCarrello">  
             
 
+        {showTachi &&
           <div className="schedaCarrello">
             <img src={urlTachipirina} className="fotoCarrello"/>
 
@@ -60,13 +39,15 @@ function Carrello() {
               <div className="testoCarrello"> Tachipirina <br></br> tot: {nTachipirina*PrezzoTachi}€</div>
 
               <div className="azioniProdottoCarrello">
-                  <Button onClick={()=>setTachi(nTachipirina-1)} variant="contained" sx={{background:"white", color:"black", border:"solid black 1px",width:"30px", height:"30px"    }}>-</Button>
+                  <Button onClick={()=>setTachi(nTachipirina-1, nTachipirina<2?setShowTachi(false):null)} variant="contained" sx={{background:"white", color:"black", border:"solid black 1px",width:"30px", height:"30px"    }}>-</Button>
                   <Button variant="contained" sx={{background:"#D9D9D9", color:"black", border:"solid black 1px",width:"40px", height:"30px"}}>{nTachipirina}</Button>
                   <Button onClick={()=>setTachi(nTachipirina+1)} variant="contained" sx={{background:"white", color:"black", border:"solid black 1px",width:"30px", height:"30px"}}>+</Button>
               </div>
             </div>
           </div>
+        }
 
+        {showInsu &&
           <div className="schedaCarrello">
             <img src={urlInsulina} className="fotoCarrello"/>
 
@@ -74,17 +55,18 @@ function Carrello() {
               <div className="testoCarrello"> Insulina <br></br> tot: {nInsu*PrezzoInsu}€</div>
 
               <div className="azioniProdottoCarrello">
-                  <Button onClick={()=>setInsu(nInsu-1)} variant="contained" sx={{background:"white", color:"black", border:"solid black 1px",width:"30px", height:"30px"    }}>-</Button>
+                  <Button onClick={()=>setInsu(nInsu-1, nInsu<2?setShowInsu(false):null)} variant="contained" sx={{background:"white", color:"black", border:"solid black 1px",width:"30px", height:"30px"    }}>-</Button>
                   <Button variant="contained" sx={{background:"#D9D9D9", color:"black", border:"solid black 1px",width:"40px", height:"30px"}}>{nInsu}</Button>
                   <Button onClick={()=>setInsu(nInsu+1)} variant="contained" sx={{background:"white", color:"black", border:"solid black 1px",width:"30px", height:"30px"}}>+</Button>
               </div>
             </div>
-          </div>    
+          </div>   
+        } 
 
-            <div className="totale">
-                <div className="testoPagamento">Totale: €</div>
-                <Button variant='contained' sx={{background:'green', marginTop:"10px"}}>Procedi al pagamento</Button>
-            </div> 
+          <div className="totale">
+              <div className="testoPagamento">Totale: {nTachipirina*PrezzoTachi+nInsu*PrezzoInsu}€</div>
+              <Button variant='contained' sx={{background:'green', marginTop:"10px"}}>Procedi al pagamento</Button>
+          </div> 
             
 
         </div>
